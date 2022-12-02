1acd3e7167a3b2f2308a49dde55d1656a71afb98
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-02 12:54:37 -0800

    refactor the functione

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,25 +1,57 @@
 import { useState } from 'react';
 
+// export default function useVisualMode(initial) {
+//   // const [mode, setMode] = useState(initial);
+//   const [history, setHistory] = useState([initial]);
+
+//   function transition(newMode, replace = false) {
+//     // setMode(newMode);
+    
+//     // if(replace) {
+//     //   setHistory(prev => [...prev.slice(0, -1)]);
+//     // }
+//     setHistory(prev => {
+//       const newHistory = [...prev];
+//       if (replace) {
+//         newHistory.pop();
+//       }
+//       newHistory.push(newMode)
+//       return newHistory;
+//     })
+
+//     // setHistory(prev => [...prev, newMode]);
+//   }
+
+//   function back() {
+//     if (history.length < 2) {
+//       return;
+//     }
+    
+//     setHistory(prev => prev.slice(0,-1))
+//     // setMode(newHistory.slice(-1));
+//     // return newHistory;
+
+//   }
+
+//   const mode = history[history.length - 1]
+//   return { mode, transition, back };
+// }
+
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    setMode(newMode);
     if(replace) {
       setHistory(prev => [...prev.slice(0, -1)]);
     }
     setHistory(prev => [...prev, newMode]);
   }
+
   function back() {
-    if (history.length > 1) {
-      setHistory(prev => {
-        prev.pop();
-        setMode(prev[prev.length-1]);
-        return [...prev];
-      })
-    }
+    if (history.length < 2) return;
+    setHistory(prev => prev.slice(0,-1))
   }
 
+  const mode = history[history.length - 1]
   return { mode, transition, back };
 }
