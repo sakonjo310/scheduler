e8e7ac973ba568c04673c3be706eb17ec93445ae
Satoe Sakonjo <sakonjo310@gmail.com>
2022-12-06 22:01:22 -0800

    Add tests for events on application component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -52,7 +52,7 @@ export default function Appointment(props) {
   }
   
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <header>{props.time}</header>
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && props.interview && (
